24a0f78ce5b9061eedf1d88e2c7bcc98f1cb8cb3
Monlo <lopezmonserrat.14@gmail.com>
2024-04-15 19:33:31 +0200

    Added changes to flask_database_mon directory in monlo branch

--- flask/flask_database_mon/models.py
+++ flask/flask_database_mon/models.py
@@ -1,13 +1,14 @@
-from .flask_app import db
+from flask_app import db
 
-class User(db.Model):
+
+class Participant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     real_name = db.Column(db.String(100), nullable=False)
     paypal_username = db.Column(db.String(100))
 
+
 class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('expenses', lazy=True))
-    description = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
     amount = db.Column(db.Float, nullable=False)
